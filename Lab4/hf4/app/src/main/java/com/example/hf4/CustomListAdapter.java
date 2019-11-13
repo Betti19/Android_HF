ab963696cc146838ae6a24e027a597ea5dff65d0
Betti19 <simonbettiii@gmail.com>
2019-11-13 12:36:14 +0200

    4.hazi

--- Lab4/hf4/app/src/main/java/com/example/hf4/CustomListAdapter.java
+++ Lab4/hf4/app/src/main/java/com/example/hf4/CustomListAdapter.java
@@ -3,9 +3,14 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeoutException;
+
+import static android.widget.AdapterView.*;
 
 
 public class CustomListAdapter<listview, listviewid> extends ArrayAdapter {
@@ -21,18 +26,22 @@ public class CustomListAdapter<listview, listviewid> extends ArrayAdapter {
 
     //to store the list of countries
     private final String[] infoArray;
+    private final String[] cumparArray;
+    private final String[] vindeArray;
 
-    public CustomListAdapter(Activity context, String[] nameArrayParam, String[] infoArrayParam, Integer[] imageIDArrayParam){
+    public CustomListAdapter(Activity context, String[] nameArrayParam, String[] infoArrayParam, Integer[] imageIDArrayParam, String[] cumparArray,String[] vindeArray){
 
         super(context, R.layout.listview_row, nameArrayParam);
         this.context=context;
         this.imageArray = imageIDArrayParam;
         this.nameArray = nameArrayParam;
         this.infoArray = infoArrayParam;
+        this.cumparArray=cumparArray;
+        this.vindeArray=vindeArray;
 
     }
 
-    public View getView(int position, View view, ViewGroup parent) {
+    public View getView(int position, final View view, ViewGroup parent) {
         LayoutInflater inflater=context.getLayoutInflater();
         View rowView=inflater.inflate(R.layout.listview_row, null,true);
 
@@ -40,14 +49,41 @@ public class CustomListAdapter<listview, listviewid> extends ArrayAdapter {
         TextView nameTextField = (TextView) rowView.findViewById(R.id.nameTextViewID);
         TextView infoTextField = (TextView) rowView.findViewById(R.id.infoTextViewID);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.imageView1ID);
+        final TextView cumparid = (TextView) rowView.findViewById(R.id.cumparid);
+        final TextView vindeid = (TextView) rowView.findViewById(R.id.vindeid);
+        final TextView textView2 = (TextView) rowView.findViewById(R.id.textView2);
+        final TextView textView3 = (TextView) rowView.findViewById(R.id.textView3);
 
         //this code sets the values of the objects to values from the arrays
         nameTextField.setText(nameArray[position]);
         infoTextField.setText(infoArray[position]);
         imageView.setImageResource(imageArray[position]);
+        cumparid.setText(cumparArray[position]);
+        vindeid.setText(vindeArray[position]);
+
+
+        rowView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(cumparid.getVisibility()==View.INVISIBLE && vindeid.getVisibility()==View.INVISIBLE && textView2.getVisibility()==View.INVISIBLE && textView3.getVisibility()==View.INVISIBLE) {
+                    cumparid.setVisibility(View.VISIBLE);
+                    vindeid.setVisibility(View.VISIBLE);
+                    textView2.setVisibility(View.VISIBLE);
+                    textView3.setVisibility(View.VISIBLE);
+                }
+                else{
+                    cumparid.setVisibility(View.INVISIBLE);
+                    vindeid.setVisibility(View.INVISIBLE);
+                    textView2.setVisibility(View.INVISIBLE);
+                    textView3.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
         return rowView;
 
     }
+
+
 
 }
--- Lab4/hf4/app/src/main/java/com/example/hf4/MainActivity.java
+++ Lab4/hf4/app/src/main/java/com/example/hf4/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.hf4;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
             "Forint maghiar"
     };
 
+    String[] cumparArray={
+            "4.6809",
+            "4.2350",
+            "5.4440",
+            "2.9072",
+            "3.2054",
+            "4.2851",
+            "0.6259",
+            "1.3968"
+    };
+    String[] vindeArray={
+            "4.8330",
+            "4.4020",
+            "5.6490",
+            "3.0144",
+            "3.3235",
+            "4.4431",
+            "0.6490",
+            "1.4483"
+    };
+
     Integer[] imageArray= {R.drawable.eur,
             R.drawable.usd,
             R.drawable.gbp,
@@ -40,12 +64,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CustomListAdapter whatever = new CustomListAdapter(this, nameArray, infoArray, imageArray);
+        CustomListAdapter whatever = new CustomListAdapter(this, nameArray, infoArray, imageArray, cumparArray,vindeArray);
         listView = (ListView) findViewById(R.id.listviewID);
         listView.setAdapter(whatever);
 
+
+
     }
-
-
-
 }
